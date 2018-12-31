269eb355204b9d30b179a3b78e1388ce01b8968b
Gethyn Jones <gethyn1@gmail.com>
2018-12-31 12:09:27 +0000

    Refactor directory structure

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.use(session({
 i18n.configure({
   locales: ['en', 'es'],
   cookie: 'locale', // Change browser cookie to change language (e.g. locale=es)
-  directory: path.join(__dirname, '../locales'),
+  directory: path.join(__dirname, 'locales'),
   defaultLocale: 'en',
   queryParameter: 'lang', // Add ?lang=es to URL to change language
   objectNotation: true,
@@ -47,11 +47,11 @@ app.use(bodyParser.json())
 // app.use(cookieParser())
 app.use(i18n.init)
 
-app.use('/assets', express.static(path.join(__dirname, '../node_modules/govuk-frontend/assets')))
-app.use('/assets', express.static(path.join(__dirname, '../assets')))
+app.use('/assets', express.static(path.resolve('node_modules/govuk-frontend/assets')))
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
 nunjucks.configure([
-  'views',
+  'src/views',
   'node_modules/govuk-frontend/',
   'node_modules/govuk-frontend/components/'
 ], {
@@ -62,7 +62,6 @@ nunjucks.configure([
 
 app.set('view engine', 'njk')
 
-// Form wizard
 formWizard(app, steps, fields)
 
 app.get('/', (req, res) => {
@@ -71,30 +70,16 @@ app.get('/', (req, res) => {
   })
 })
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-  if (!req.body.name) {
-    return res.render('index', {
-      title: res.__('title'),
-      errorMessage: { text: 'Enter a name' },
-    })
-  }
-
-  req.session.name = req.body.name
-  res.redirect('confirm')
-})
-
 app.get('/confirm', (req, res) => {
   res.render('confirm', {
     title: res.__('title'),
     applicant: {
-      name: req.session.name,
+      firstName: req.session.firstName,
     }
   })
 })
 
 app.get('/complete', (req, res) => {
-  console.log('>>>>>>', req.session.name)
   res.render('complete')
 })
 
